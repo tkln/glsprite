5693a662832de7ef60cd840480fe7e8b218a7235
Aapo Vienamo <aapo.vienamo@iki.fi>
2019-03-04 00:57:55 +0200

    vs.glsl: Clean up the quad transformation

--- vs.glsl
+++ vs.glsl
@@ -8,9 +8,9 @@ layout(location = 1) in vec2 sprite_pos;
 void main() {
     const float scale = 0.25f;
     float aspect_ratio = screen_size.x / screen_size.y;
+    vec2 qvp = quad_vert_pos.xy * vec2(1, aspect_ratio) * scale;
+    vec2 sp = sprite_pos / (screen_size * 0.5f) - 1.0f;
 
-    gl_Position = vec4(quad_vert_pos.xy * vec2(1, aspect_ratio) * scale +
-                       sprite_pos / (screen_size * 0.5f) - 1.0f,
-                       quad_vert_pos.z, 1.0f);
+    gl_Position = vec4(qvp + sp, quad_vert_pos.z, 1.0f);
 }
 
