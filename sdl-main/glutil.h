5cae3cb8cc479c31beff96612d1a1afbab51dae7
Aapo Vienamo <aapo.vienamo@iki.fi>
2019-07-21 20:24:44 +0300

    sdl-main/glutil: Add extern "C"

--- sdl-main/glutil.h
+++ sdl-main/glutil.h
@@ -3,6 +3,10 @@
 
 #include <GL/gl.h>
 
+#if __cplusplus
+extern "C" {
+#endif
+
 /* Returns the shader id upon success and 0 on failure. */
 GLuint glutil_compile_shader_file(const char *path, GLenum shader_type);
 
@@ -15,5 +19,9 @@ GLuint glutil_compile_shader(const char *const src, GLint *src_len,
  * Return the shader program id upon success and 0 on failure.
  */
 GLuint glutil_link_shaders(GLuint shader_prog_id, GLuint shader_id_0, ...);
+
+#if __cplusplus
+}
+#endif
 
 #endif
